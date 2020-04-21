1fcf15a84dd8dd4b2e5c413136443b42ce7f7ba0
Faisal Amin <faisal.nstu@ymail.com>
2020-04-21 21:24:40 +0600

    rename

---
+++ Pomoh/UserControls/DialMarkers.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Pomoh.UserControls
+{
+    /// <summary>
+    /// Interaction logic for DialMarkers.xaml
+    /// </summary>
+    public partial class DialMarkers : UserControl
+    {
+        public DialMarkers()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PomodoroWpf
-A Pomodor Timer app made with WPF.
+# Pomoh
+A Pomodoro Timer app made with WPF.
 The calculation of rotatation angle of the dial is based on this tutorial:
 https://www.c-sharpcorner.com/Resources/894/automatically-resize-a-window-to-fit-content-in-wpf.aspx
